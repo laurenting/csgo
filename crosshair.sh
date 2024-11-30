cbec1e3cd33d6481339b1bbdefc1c53c08e56bb8
laurent <laurent0816@outlook.com>
2024-11-30 23:37:47 +0800

    crosshair update

---
+++ crosshair.sh
@@ -0,0 +1,18 @@
+//CROSSHAIR COMMANDS
+cl_crosshairstyle 4
+cl_crosshairsize 1
+cl_crosshairthickness 0.5
+cl_crosshairgap -4
+cl_crosshair_drawoutline 0
+cl_crosshairdot 0
+cl_crosshair_t 0
+cl_crosshairusealpha 1
+cl_crosshairalpha 255
+cl_crosshair_recoil 0
+cl_crosshairgap_useweaponvalue 0
+cl_crosshaircolor 5
+
+//EXTRAS
+cl_crosshair_sniper_width 2
+hud_showtargetid 0
+cl_hud_color 2
